ab2bcd0673833ee847995c5e53800f120196fdfc
snerks <martin.github@outlook.com>
2020-04-27 10:21:49 +0100

    Adjust catch-all route

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,21 @@ import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import { blue, green, amber, blueGrey, red, cyan, indigo, lightBlue, grey, teal } from "@material-ui/core/colors";
 
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import Home from "./components/Home";
 import GigList from "./components/GigList";
 
 const App: React.FC = () => {
-
   const [isDarkMode, setIsDarkMode] = useState(true);
 
   const theme = createMuiTheme({
     palette: {
       type: isDarkMode ? "dark" : "light",
       primary: {
-        main: '#0b5994',
+        main: "#0b5994",
       },
       secondary: {
-        main: '#1d83c6',
+        main: "#1d83c6",
       },
     },
   });
@@ -35,15 +34,32 @@ const App: React.FC = () => {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           {/* <Route path="/" exact component={Home} /> */}
-          <Route path="/" exact render={() => <Home isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} />
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <Home isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+            )}
+          />
 
-          <Route path="/giglist/:showPastEvents/:days?" exact render={() => <GigList isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} />
+          <Route
+            path="/giglist/:showPastEvents/:days?"
+            exact
+            render={() => (
+              <GigList isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+            )}
+          />
           {/* <Route path="/" render={() => <div>404</div>} /> */}
-          <Route path="/" render={() => <Home isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} />
+          <Route
+            path="*"
+            render={() => (
+              <Home isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+            )}
+          />
         </Switch>
       </BrowserRouter>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/GigDetail.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const GigDetail: React.FC = () => {
+  return <h1>Gig Details</h1>;
+};
+
+export default GigDetail;
