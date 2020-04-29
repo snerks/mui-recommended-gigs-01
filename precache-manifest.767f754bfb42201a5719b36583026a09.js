d44d74c0f8ce3438ff7df40fbd0ab935c803774e
snerks <martin.github@outlook.com>
2020-04-29 18:06:21 +0100

    Updates

--- precache-manifest.767f754bfb42201a5719b36583026a09.js
+++ precache-manifest.767f754bfb42201a5719b36583026a09.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "09ef22601122136a8a784683aefbe599",
+    "revision": "3acac754c54b9e5eab3b434cd8142790",
     "url": "/mui-recommended-gigs-01/index.html"
   },
   {
-    "revision": "8a6bd9362db3325d6617",
+    "revision": "a7ae0ce170f70f668a73",
     "url": "/mui-recommended-gigs-01/static/css/main.69eda509.chunk.css"
   },
   {
-    "revision": "452ded24719e0725a267",
-    "url": "/mui-recommended-gigs-01/static/js/2.aa06485b.chunk.js"
+    "revision": "833cdb15dd0995d47fba",
+    "url": "/mui-recommended-gigs-01/static/js/2.bafc9177.chunk.js"
   },
   {
     "revision": "01f28827afd20553593a551e1ee6c97b",
-    "url": "/mui-recommended-gigs-01/static/js/2.aa06485b.chunk.js.LICENSE.txt"
+    "url": "/mui-recommended-gigs-01/static/js/2.bafc9177.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8a6bd9362db3325d6617",
-    "url": "/mui-recommended-gigs-01/static/js/main.083413c5.chunk.js"
+    "revision": "a7ae0ce170f70f668a73",
+    "url": "/mui-recommended-gigs-01/static/js/main.a3742cbc.chunk.js"
   },
   {
     "revision": "5b91656340bf4843c099",
